16ebaad619a07a68b00a844c3d05ba14db7d2e22
Hayden James Cooper <haydencpr95@gmail.com>
2023-04-25 14:46:04 -0400

    Server post update and error chekcing/ logs

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const Book = require('./models/books');
 
 const app = express();
 app.use(cors());
+app.use(express.json()); // Enable JSON body parsing for POST requests
 
 const PORT = process.env.PORT || 3002;
 
@@ -22,11 +23,33 @@ db.once('open', function () {
 });
 
 app.get('/books', async (request, response, next) => {
-
   try {
-    let allBooks = await Book.find({});
+    const allBooks = await Book.find({});
     response.status(200).send(allBooks);
   } catch (error) {
+    next(error);
+  }
+});
+
+app.post('/books', async (request, response, next) => {
+  try {
+    const { title, description, status } = request.body;
+
+    // Check if any required properties are missing from the request body
+    if (!title || !description || !status) {
+      return response.status(400).json({ error: 'Missing required properties' });
+    }
+
+    const newBook = new Book({
+      title: title,
+      description: description,
+      status: status
+    });
+    const savedBook = await newBook.save();
+    response.status(201).json(savedBook);
+  } catch (error) {
+    // If there's an error saving the book to the database, return a 500 Internal Server Error status code
+    response.status(500).json({ error: error.message });
     next(error);
   }
 });
